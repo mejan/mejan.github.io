c0edb897137fc6d83bedb883cdc3a07fcb7b45a4
Secret <mxc_lol@hotmail.com>
2018-03-10 15:20:17 +0100

    update config page

--- search/searchdata.js
+++ search/searchdata.js
@@ -1,21 +1,24 @@
 var indexSectionsWithContent =
 {
-  0: "abcdgimprstv",
+  0: "abcdgimoprstv",
   1: "cgmprt",
-  2: "abcdgimrsv"
+  2: "abcdgimrsv",
+  3: "o"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "classes",
-  2: "functions"
+  2: "functions",
+  3: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Classes",
-  2: "Functions"
+  2: "Functions",
+  3: "Pages"
 };
 
